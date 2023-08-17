6ce915f94a84f8a893719ae2170c73deda0cb2d1
Quaniscoding <vq.2509.2003@gmail.com>
2023-08-17 14:49:39 +0700

    add 1 project

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
-import { github } from "../assets";
+import { github, vercel } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
