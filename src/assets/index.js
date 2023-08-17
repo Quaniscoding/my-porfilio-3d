528f66d216ebcda9dbb500d7866dbdc41dbf08c7
Quaniscoding <vq.2509.2003@gmail.com>
2023-08-17 14:28:18 +0700

    add code

---
+++ src/assets/index.js
@@ -0,0 +1,60 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import vercel from "./vercel.svg"
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import cybersoft from './company/cybersoft.png'
+import shortly from "./shortly.jpg";
+import restCountries from "./Rest-countries.jpg";
+import calculator from './calculator.jpg'
+import jira from './jira-project.png'
+import bnb from './bnb.png'
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  vercel,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  shortly,
+  restCountries,
+  calculator,
+  cybersoft,
+  jira,
+  bnb
+};
---
+++ src/constants/index.js
@@ -0,0 +1,234 @@
+import {
+    backend,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    git,
+    docker,
+    shortly,
+    restCountries,
+    cybersoft,
+    calculator,
+    jira,
+    bnb
+} from "../assets";
+
+export const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "React Developer",
+        icon: web,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    }
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "TypeScript",
+        icon: typescript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "docker",
+        icon: docker,
+    },
+];
+
+const experiences = [
+    {
+        title: "FullStack developer",
+        company_name: "CyberSoft",
+        icon: cybersoft,
+        iconBg: "#383E56",
+        date: "May 2021 - December 2021",
+        points: [
+            "Intern Front-end developers"
+        ],
+    }
+];
+
+const projects = [
+    {
+        name: "Shortly Link",
+        description:
+            "Web-based platform that allows users to shortly their link.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: shortly,
+        source_code_link: "https://github.com/Quaniscoding/shortly-link",
+        demo_code_link: "https://shortly-link-chi.vercel.app/HomePage"
+    },
+    {
+        name: "Jira",
+        description:
+            " The web application allows users to plan, manage members, manage tasks, and divide tasks.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "boostrap",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "antd",
+                color: "yellow-text-gradient",
+            },
+        ],
+        image: jira,
+        source_code_link: "https://github.com/Quaniscoding/project-jira-bc34",
+        demo_code_link: "https://project-jira-bc34.vercel.app/login"
+    },
+
+    {
+        name: "Calculator App",
+        description:
+            "Web application that allows users",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "css",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: calculator,
+        source_code_link: "https://github.com/Quaniscoding/calculator-app-main",
+        demo_code_link: "https://calculator-app-main-psi.vercel.app/"
+    },
+    {
+        name: "API airBnb",
+        description:
+            "Api for web Airbnb",
+        tags: [
+            {
+                name: "NodeJs",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Express.js",
+                color: "yellow-text-gradient",
+            },
+            {
+                name: "Prisma",
+                color: "text-blue-900",
+            },
+            {
+                name: "MySql",
+                color: "",
+            },
+            {
+                name: "Swagger",
+                color: "green-text-gradient",
+            },
+        ],
+        image: bnb,
+        source_code_link: "https://github.com/Quaniscoding/capston-node-25",
+        demo_code_link: ""
+    },
+    {
+        name: "Search countries",
+        description:
+            "Web application that allows users to search by name and by continent of countries around the world.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "restapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "tailwind",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: restCountries,
+        source_code_link: "https://github.com/Quaniscoding/rest-countries-with-color-theme",
+        demo_code_link: "https://rest-countries-with-color-theme-zeta.vercel.app/HomePage"
+    },
+];
+
+export { services, technologies, experiences, projects };
