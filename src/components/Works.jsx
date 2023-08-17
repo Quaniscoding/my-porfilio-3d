916e73787ef0316fd37aadef84adff2808ce38a3
Quaniscoding <vq.2509.2003@gmail.com>
2023-08-17 14:47:47 +0700

    edit my project code

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,13 +1,13 @@
-import { Tilt } from "react-tilt";
-
+import React from "react";
+import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-import { styles } from "../styles";
 
+import { styles } from "../styles";
 import { github } from "../assets";
-import { vercel } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+
 const ProjectCard = ({
   index,
   name,
@@ -30,14 +30,13 @@ const ProjectCard = ({
         <div className="relative w-full h-[230px]">
           <img
             src={image}
-            alt={name}
+            alt="project_image"
             className="w-full h-full object-cover rounded-2xl"
           />
+
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
-              onClick={() => {
-                window.open(source_code_link, "_blank");
-              }}
+              onClick={() => window.open(source_code_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
@@ -61,21 +60,24 @@ const ProjectCard = ({
             </div>
           </div>
         </div>
+
         <div className="mt-5">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
-        <dir className="mt-4 flex flex-wrap gap-2">
+
+        <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <p key={`tag-${tag.name}`} className={`text-[14px] ${tag.color}`}>
               {tag.name}
             </p>
           ))}
-        </dir>
+        </div>
       </Tilt>
     </motion.div>
   );
 };
+
 const Works = () => {
   return (
     <>
@@ -95,9 +97,10 @@ const Works = () => {
           and manage projects effectively.
         </motion.p>
       </div>
-      <div className="mt-20 flex flex-wrap gap-7 ">
+
+      <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
-          <ProjectCard key={`Project-${index}`} index={index} {...project} />
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
       </div>
     </>
